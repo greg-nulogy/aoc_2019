fd4de08175a8a97c3368da0bbb18977bb29e5420
Greg Matthews <gregm@nulogy.com>
2023-03-06 21:19:53 -0800

    Day 1: Part 1: Refactor run method
    - similar levels of abstraction
    - improve names
    - newspaper style

--- lib/aoc_2019/day1/part1.rb
+++ lib/aoc_2019/day1/part1.rb
@@ -4,15 +4,22 @@ module Aoc2019
   module Day1
     class Part1
       def run(input)
+        masses = parse_module_masses(input)
+        fuel_required = calculate_fuel_required(masses)
+
+        fuel_required.sum
+      end
+
+      private
+
+      def parse_module_masses(input)
         input
           .lines(chomp: true)
           .map(&:to_i)
-          .map(&method(:calculate_fuel_required))
-          .sum
       end
 
-      def calculate_fuel_required(mass)
-        (mass / 3).floor - 2
+      def calculate_fuel_required(masses)
+        masses.map { |mass| (mass / 3).floor - 2 }
       end
     end
   end

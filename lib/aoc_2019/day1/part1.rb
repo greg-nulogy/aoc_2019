ef2db0c94dc81cba3dbaf9a70aec7d4d32bb16fa
Greg Matthews <gregm@nulogy.com>
2023-03-06 21:10:54 -0800

    Day 1: Part 1: Get it working on the examples

--- lib/aoc_2019/day1/part1.rb
+++ lib/aoc_2019/day1/part1.rb
@@ -3,9 +3,18 @@
 module Aoc2019
   module Day1
     class Part1
-      def run
-        42
+      def run(input)
+        input
+          .lines(chomp: true)
+          .map(&:to_i)
+          .map(&method(:calculate_fuel_required))
+          .sum
+      end
+
+      def calculate_fuel_required(mass)
+        (mass / 3).floor - 2
       end
     end
   end
 end
+
--- spec/day1/part1_spec.rb
+++ spec/day1/part1_spec.rb
@@ -4,7 +4,19 @@ require "aoc_2019/day1/part1"
 
 RSpec.describe Aoc2019::Day1::Part1 do
   subject { Aoc2019::Day1::Part1.new }
-  it "should " do
-    expect(subject.run).to eq(42)
+  it "should return 2 for a mass of 12" do
+    expect(subject.run("12")).to eq(2)
+  end
+
+  it "should return 2 for a mass of 14" do
+    expect(subject.run("14")).to eq(2)
+  end
+
+  it "should return 654 for a mass of 1969" do
+    expect(subject.run("1969")).to eq(654)
+  end
+
+  it "should return the sum of fuel for a list of modules" do
+    expect(subject.run("12\n14")).to eq(4)
   end
 end

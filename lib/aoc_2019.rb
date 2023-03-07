3e8e9dc4829a130f085ac70fd8640ec67bf9c14c
Greg Matthews <gregm@nulogy.com>
2023-03-06 20:35:08 -0800

    Project initialization

---
+++ lib/aoc_2019.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "aoc_2019/version"
+
+module Aoc2019
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/aoc_2019_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Aoc2019 do
+  it "has a version number" do
+    expect(Aoc2019::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(1 == 1).to eq(true)
+  end
+end
